b0f6b1987092490f1879f3cd51bb4a67a35be468
Graphicslave <32586838+clorose@users.noreply.github.com>
2022-03-03 23:30:11 +0900

    [Silver IV] Title: 큐, Time: 80 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,42 @@
+import sys
+def push(x):
+    stack.append(x)
+def pop():
+    if(not stack):
+        return -1
+    else:
+        return stack.pop(0)
+def size():
+    return len(stack)
+def empty():
+    if(not stack):
+        return 1
+    else:
+        return 0
+def front():
+    if(not stack):
+        return -1
+    else:
+        return stack[0]
+def back():
+    if(not stack):
+        return -1
+    else:
+        return stack[-1]
+N = int(input())
+stack = list()
+
+for i in range(N):
+    command = sys.stdin.readline().rstrip().split()
+    if command[0] == "push":
+        push(command[1])
+    elif command[0] == "pop":
+        print(pop())
+    elif command[0] == "size":
+        print(size())
+    elif command[0] == "empty":
+        print(empty())
+    elif command[0] == "front":
+        print(front())
+    elif command[0] == "back":
+        print(back())
