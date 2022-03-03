43daf3016405daa46e3391329e1d2bfc37995938
Graphicslave <32586838+clorose@users.noreply.github.com>
2022-03-03 23:12:22 +0900

    Update 스택.py

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,45 +1,35 @@
 import sys
-
-# 정수 X를 스택에 넣는 연산이다.
 def push(x):
     stack.append(x)
-
-# 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 
-# 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 def pop():
     if(not stack):
         return -1
     else:
         return stack.pop()
-
-# 스택에 들어있는 정수의 개수를 출력한다.
 def size():
     return len(stack)
-
-# 스택이 비어있으면 1, 아니면 0을 출력한다.
 def empty():
-    return 0 if stack else 1
-
-# 스택의 가장 위에 있는 정수를 출력한다. 
-# 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    if(not stack):
+        return 1
+    else:
+        return 0
 def top():
-    return stack[-1] if stack else -1
+    if(not stack):
+        return -1
+    else:
+        return stack[-1]
+N = int(input())
+stack = list()
 
-N = int(sys.stdin.readline().rstrip())
-stack = []
-
-for _ in range(N):
-    input_split = sys.stdin.readline().rstrip().split()
-
-    order = input_split[0]
-
-    if order == "push":
-        push(input_split[1])
-    elif order == "pop":
+for i in range(N):
+    command = sys.stdin.readline().rstrip().split()
+    if command[0] == "push":
+        push(command[1])
+    elif command[0] == "pop":
         print(pop())
-    elif order == "size":
+    elif command[0] == "size":
         print(size())
-    elif order == "empty":
+    elif command[0] == "empty":
         print(empty())
-    elif order == "top":
+    elif command[0] == "top":
         print(top())
