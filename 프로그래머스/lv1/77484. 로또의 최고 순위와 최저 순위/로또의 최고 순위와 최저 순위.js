52ddb658ab436ec81fa52a9ba8d5efcf949516c9
Stray Bear <gohan9511@gmail.com>
2022-09-28 15:40:15 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,9 +1,4 @@
 function solution(lottos, win_nums) {
     var answer = [];
-    let winning = win_nums.filter(it=>lottos.includes(it)).length;
-    let zero = lottos.filter(lotto =>lotto === 0).length;
-    let min = 7-winning >=6 ? 6: 7-winning;
-    let max = min-zero < 1 ? 1 : min-zero;
-    answer = [max,min]
     return answer;
 }
