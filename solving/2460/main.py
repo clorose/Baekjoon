511fff79246469b746112f9b94f9ad397ac1e08c
Stray Bear <gohan9511@gmail.com>
2025-05-02 00:58:55 +0900

    ✨ Fix : Fix solution for problem 2460
    For문 숫자 수정(9번 반복->10번)

--- solving/2460/main.py
+++ solving/2460/main.py
@@ -1,7 +1,7 @@
 passenger = 0
 max_passenger = 0
 
-for i in range(1, 10):
+for i in range(10):
     train_out, train_in = map(int, input().split())
     passenger += train_in - train_out
     if passenger > max_passenger:
